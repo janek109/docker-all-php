645cab09023326fe0b9b902aa85abd83ce3f2acd
Jan Szewczak <janek109@poczta.fm>
2020-10-25 19:19:30 +0100

    rename test to tests

--- tests/test.sh
+++ tests/test.sh
@@ -1,8 +1,8 @@
-docker exec -it php54 php test/test.php 5.4
-docker exec -it php55 php test/test.php 5.5
-docker exec -it php56 php test/test.php 5.6
-docker exec -it php70 php test/test.php 7.0
-docker exec -it php71 php test/test.php 7.1
-docker exec -it php72 php test/test.php 7.2
-docker exec -it php73 php test/test.php 7.3
-docker exec -it php74 php test/test.php 7.4
+docker exec -it php54 php tests/test.php 5.4
+docker exec -it php55 php tests/test.php 5.5
+docker exec -it php56 php tests/test.php 5.6
+docker exec -it php70 php tests/test.php 7.0
+docker exec -it php71 php tests/test.php 7.1
+docker exec -it php72 php tests/test.php 7.2
+docker exec -it php73 php tests/test.php 7.3
+docker exec -it php74 php tests/test.php 7.4
