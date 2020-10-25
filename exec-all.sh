d06fb4375d726bf49e9625366a92c2b8caec8259
Jan Szewczak <janek109@poczta.fm>
2020-10-25 15:57:08 +0100

    add exec all script

---
+++ exec-all.sh
@@ -0,0 +1,17 @@
+
+echo php54 ========================
+docker exec -it php54 php index.php
+echo php55 ========================
+docker exec -it php55 php index.php
+echo php56 ========================
+docker exec -it php56 php index.php
+echo php70 ========================
+docker exec -it php70 php index.php
+echo php71 ========================
+docker exec -it php71 php index.php
+echo php72 ========================
+docker exec -it php72 php index.php
+echo php73 ========================
+docker exec -it php73 php index.php
+echo php74 ========================
+docker exec -it php74 php index.php
